1e7401ce16c43a71b2f19e704b8855d780b6593c
Jonah Mark Reel <jonahreel97@gmail.com>
2020-10-20 19:36:10 -0400

    added simple routing to backend

---
+++ routes/songs.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const ObjectId = require('mongodb').ObjectId;
+var router = express.Router();
+
+// another you
+// http://localhost:3000/songs/5f8e5afbe583a92a8c7bb179
+
+router.get('/id/:songId', async (req, res) => {
+	const db = req.app.get("db");
+	const songs = db.collection('Song');
+	const o_id = new ObjectId(req.params.songId);
+	const data = await songs.findOne({_id: o_id});
+	res.send(data);
+});
+
+router.get('/', async (req, res) => {
+	const db = req.app.get("db");
+	const songs = db.collection('Song');
+	const data = await songs.find({}, { projection: { _id: 1, name: 1} }).toArray();
+	res.send(data);
+});
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-app.use(cors())
+const songs = require('./routes/songs');
+var bodyParser = require('body-parser')
+app.use(cors());
 
 app.listen(3000, async function() {
     console.log('listening on 3000');
+
+    // connect to db:
     const {MongoClient} = require('mongodb');
     const uri = "mongodb://localhost:27017/playalong-project";
     let client = new MongoClient(uri);
-
     await client.connect();
     const db = client.db("playalong-project");
+    app.set('db', db);
 
-    app.get('/', async (req, res) => {
-        const songs = db.collection('Song');
-        const data = await songs.findOne({name: "There Will Never Be Another You"});
-        res.send(data);
-    });
+    // configure middlewares:
+    app.use(bodyParser.json());
+
+    // configure routes:
+    app.use('/songs', songs);
 });
