7177880c92bd87e8ce8ee64b503bc4ca831d7037
Jonah Mark Reel <jonahreel97@gmail.com>
2020-11-13 18:51:05 -0500

    added song processing scripts and song files

---
+++ scripts/pdfToText.js
@@ -0,0 +1,65 @@
+const pdfjsLib = require("../pdfjs-dist/es5/build/pdf.js");
+const fs = require('fs');
+const outputDir = "./intermediateFiles";
+
+const inputPDF = process.argv[2];
+
+(async function GetTextFromPDF(path) {
+    let doc = await pdfjsLib.getDocument(path).promise;
+    let page1 = await doc.getPage(1);
+    let content = await page1.getTextContent();
+    let strings = content.items.map(function(item) {
+        return item.str;
+    });
+
+    let data = "".concat(strings);
+    data = data.substr(3);
+
+    let tokenMap = {
+        "44": "",
+        "&c": "",
+        ",": "",
+        "’’’’": "|",
+        "ÕÕÕÕ": "|",
+        "bbb": "",
+        "A": "A",
+        "B": "B",
+        "C": "C",
+        "D": "D",
+        "E": "E",
+        "F": "F",
+        "G": "G",
+        "#": "#",
+        "b": "b",
+        "Maj7": "maj7",
+        "-7": "min7",
+        "m7": "min7",
+        "o": "dim7",
+        "7": "7",
+        "-7b5": "min7b5",
+        "(b5)": "b5",
+        "(b9)": "b9",
+        "||": "|%|",
+        "||": "|%|",
+        "&": "",
+        "##": "",
+        ".": "",
+        " ": ""
+    };
+
+    for (const token in tokenMap) {
+        data = data.split(token).join(tokenMap[token]).replace(token, tokenMap[token]);
+    }
+    return data;
+})(inputPDF).then(data => {
+    while (data.includes("|")) {
+        data = data.replace("|", "\n")
+    }
+    fs.writeFile(`${__dirname}/../${outputDir}/${inputPDF.replace(".pdf", "")}.txt`, data, (err) => {
+        if (err) {
+            console.log(`failed to save:\n${err}`);
+        } else {
+            console.log(`finished making intermediate file for '${inputPDF}'`);
+        }
+    });
+});
---
+++ scripts/songTextToJson.js
@@ -0,0 +1,133 @@
+const fs = require('fs');
+const dirname = "./intermediateFiles";
+const outputDir = "./jsonFiles";
+
+function createSongFromData(filename, data) {
+    const lines = data.split('\n');
+    const sections = [];
+    let measures = null;
+    let section = null;
+    let endings = null;
+    let name = filename;
+
+    const numberOfBeats = 4;
+    const style = "swing";
+    let originalMeasureNumber = 1;
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i];
+
+        // name of the tune:
+        if (line.startsWith("!")) {
+            name = line.substring(1);
+        }
+
+        // beginning of a new section:
+        else if (line.startsWith("=")) {
+            if (section) {
+                section.measures = measures;
+                if (endings) {
+                    endings.push(ending)
+                    section.endings = endings;
+                    endings = [];
+                }
+            }
+
+            measures = [];
+            endings = null;
+
+            if (section) {
+                // if we are working on a section right now, push it onto the list
+                sections.push(section);
+            }
+
+            section = { name: line.substring(1), repeat: false, numRepeats: 0, measures: [] };
+
+            // a new ending
+        } else if (line === "-") {
+
+            // if we are still on measures, start a new ending
+            if (!endings) {
+                endings = [];
+                ending = [];
+            } else {
+                endings.push(ending);
+                ending = [];
+            }
+            section.repeat = true;
+            section.numRepeats++;
+
+            // a new measure
+        } else {
+            const chords = [];
+            const rawChords = line.split(" ");
+            const parsedChords = [];
+            const cnt = 4;
+            for (let c of rawChords) {
+                const root = c.match(/^[ABCDEFG][#b]*/gm)[0];
+                const type = c.replace(root, "");
+                parsedChords.push([root, type]);
+            }
+            for (let i = 0; i < cnt; i++) {
+                if (parsedChords.length > 1 && i > 1) {
+                    chords.push(parsedChords[1]);
+                } else {
+                    chords.push(parsedChords[0]);
+                }
+            }
+
+            const measure = {
+                arrangementMeasureNumber: null,
+                originalMeasureNumber: originalMeasureNumber,
+                style: style,
+                chords: chords,
+                numberOfBeats: numberOfBeats,
+                nextMeasure: null
+            };
+            originalMeasureNumber++;
+            if (!endings) {
+                measures.push(measure);
+            } else {
+                ending.push(measure);
+            }
+        }
+    }
+
+    // add final section
+
+    section.measures = measures;
+    if (endings) {
+        endings.push(ending)
+        section.endings = endings;
+    }
+    sections.push(section);
+
+    return {
+        name: name,
+        sections: sections
+    };
+}
+
+
+fs.readdir(dirname, function(err, filenames) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    filenames.forEach(function(filename) {
+        fs.readFile(dirname + "/" + filename, 'utf-8', function(err, content) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            const songObj = createSongFromData(filename, content);
+            fs.writeFile(`${__dirname}/../${outputDir}/${filename.replace(".txt", "")}.json`, JSON.stringify(songObj, null, 4), (err) => {
+                if (err) {
+                    console.log(`failed to save:\n${err}`);
+                } else {
+                    console.log(`finished making json file for '${filename}'`);
+                }
+            });
+        });
+    });
+});
