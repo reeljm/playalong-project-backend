0d15febd4beb51f54e11d05721acc8b1a1020de7
Jonah Reel <jonareel97@gmail.com>
2020-11-18 06:06:43 +0000

    removed https from server.js

--- server.js
+++ server.js
@@ -4,38 +4,22 @@ const cors = require('cors');
 const songs = require('./routes/songs');
 var bodyParser = require('body-parser')
 app.use(cors());
-const fs = require('fs');
-const https = require('https');
 
-var privateKey = fs.readFileSync('./keys/key.pem');
-var certificate = fs.readFileSync('./keys/cert.pem');
+app.listen(3000, async function() {
+    console.log('listening on 3000');
 
-https.createServer({
-    key: privateKey,
-    cert: certificate
-}, app).listen(3000);
+    // connect to db:
+    const {MongoClient} = require('mongodb');
+    const uri = "mongodb://localhost:27017/playalong-project";
+    let client = new MongoClient(uri);
+    await client.connect();
+    const db = client.db("playalong-project");
+    app.set('db', db);
 
-const main = (async () => {
+    // configure middlewares:
+    app.use(bodyParser.json());
 
+    // configure routes:
+    app.use('/songs', songs);
+});
 
-
-
-
-
-// connect to db:
-const {MongoClient} = require('mongodb');
-const uri = "mongodb://localhost:27017/playalong-project";
-let client = new MongoClient(uri);
-await client.connect();
-const db = client.db("playalong-project");
-app.set('db', db);
-
-// configure middlewares:
-app.use(bodyParser.json());
-
-// configure routes:
-app.use('/songs', songs);
-
-
-
-})();
