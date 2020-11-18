46e32cd28ec01ea11c60c897690044737252aa6f
Jonah Reel <jonareel97@gmail.com>
2020-11-18 05:36:09 +0000

    intermediate work when converting to https

--- server.js
+++ server.js
@@ -4,21 +4,38 @@ const cors = require('cors');
 const songs = require('./routes/songs');
 var bodyParser = require('body-parser')
 app.use(cors());
+const fs = require('fs');
+const https = require('https');
 
-app.listen(3000, async function() {
-    console.log('listening on 3000');
+var privateKey = fs.readFileSync('./keys/key.pem');
+var certificate = fs.readFileSync('./keys/cert.pem');
 
-    // connect to db:
-    const {MongoClient} = require('mongodb');
-    const uri = "mongodb://localhost:27017/playalong-project";
-    let client = new MongoClient(uri);
-    await client.connect();
-    const db = client.db("playalong-project");
-    app.set('db', db);
+https.createServer({
+    key: privateKey,
+    cert: certificate
+}, app).listen(3000);
 
-    // configure middlewares:
-    app.use(bodyParser.json());
+const main = (async () => {
 
-    // configure routes:
-    app.use('/songs', songs);
-});
+
+
+
+
+
+// connect to db:
+const {MongoClient} = require('mongodb');
+const uri = "mongodb://localhost:27017/playalong-project";
+let client = new MongoClient(uri);
+await client.connect();
+const db = client.db("playalong-project");
+app.set('db', db);
+
+// configure middlewares:
+app.use(bodyParser.json());
+
+// configure routes:
+app.use('/songs', songs);
+
+
+
+})();
