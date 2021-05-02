76e6147b5984704328e86c82c5f25f1dec152a54
Jonah Mark Reel <jonahreel97@gmail.com>
2021-05-02 18:34:12 -0400

    fixed hanging bug in DB migration script

--- scripts/loadDB.js
+++ scripts/loadDB.js
@@ -2,25 +2,37 @@ const {MongoClient} = require('mongodb');
 const fs = require('fs').promises;
 const dirname = "./json-files";
 
-async function main() {
+async function loadDB() {
     const uri = "mongodb://localhost:27017/playalong-project"
     let client = new MongoClient(uri);
+    console.log("Connecting to DB...");
     await client.connect();
     const db = client.db("playalong-project");
+    console.log("Successfully connected to DB");
 
     await db.collection('Song').deleteMany();
 
     const filenames = await fs.readdir(dirname);
 
-    await filenames.forEach(async function(filename) {
-        const content = await fs.readFile(dirname + "/" + filename, 'utf-8');
-        const data = JSON.parse(content);
-        try {
-            await db.collection('Song').insertOne(data);
-        } catch (e) {
-            console.error(e);
-        }
+    const promises = filenames.map(function(filename) {
+        console.log("Uploading song  " + dirname + "/" + filename + " to DB");
+        return new Promise(async (resolve, reject) => {
+            try {
+                const content = await fs.readFile(dirname + "/" + filename, 'utf-8');
+                const data = JSON.parse(content);
+                await db.collection('Song').insertOne(data);
+                resolve();
+            } catch (e) {
+                console.log("   Failed to move " + dirname + "/" + filename);
+                console.error(e);
+                reject();
+            }
+        });
     });
+    await Promise.all(promises);
+    console.log("Finished uploading songs, closing DB connection");
+    await client.close();
+    console.log("DB connection succesfully closed");
 }
 
-main().catch(console.error);
+loadDB();
